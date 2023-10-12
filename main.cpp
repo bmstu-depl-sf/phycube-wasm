79cd8d1d85eeaa9a75a99d441f0b4d5c65fff5f5
DaniilChizhevskii <daniilchizhevskij@gmail.com>
2023-10-12 11:55:45 +0300

    Show cube, vector & change camera mode on click

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include "raylib.h"
 #include <cmath>
+#include <vector>
 
-// Example Entity class
-//--------------------------------------------------------------------------------------
-class RotatingSphere {
+// Some vector funcs
+Vector3 sum(Vector3 a, Vector3 b) {
+    Vector3 c = {a.x + b.x, a.y + b.y, a.z + b.z};
+    return c;
+}
+
+Vector3 mul(Vector3 a, float b) {
+    Vector3 c = {a.x * b, a.y * b, a.z * b};
+    return c;
+}
+
+// Simple point class
+class Point {
 private:
-    float angle, traectoryRadius, traectorySpeed;
+    Vector3 speed, boost; 
 public:
     float radius;
     Color color;
     Vector3 position;
 
-    RotatingSphere(Color color, float radius = 1.0f, float angle = 0, float traectoryRadius = 0, float traectorySpeed = 0):
-        angle(angle), traectoryRadius(traectoryRadius), traectorySpeed(traectorySpeed), radius(radius), color(color), position() {};
-    
+    Point(Color color, float radius = 1.0f, Vector3 position = {0, 0, 0}, Vector3 speed = {0, 0, 0}, Vector3 boost = {0, 0, 0}):
+        speed(speed), boost(boost), radius(radius), color(color), position(position) {};
+
     void move() {
-        angle += traectorySpeed;
-        position.x = sinf(angle) * traectoryRadius;
-        position.y = cosf(angle) * traectoryRadius;
+        speed = sum(speed, boost);
+        position = sum(position, speed);
     }
 };
 
@@ -27,24 +37,20 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 1200;
     const int screenHeight = 600;
-
     InitWindow(screenWidth, screenHeight, "raylib example");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
 
     // Define the camera to look into our 3d world
     Camera camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.position = (Vector3){ 20.0f, 0.0f, 0.0f };   // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.up = (Vector3){ 0.0f, 0.0f, 1.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 50.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
-    RotatingSphere spheres[] = {
-        RotatingSphere(RED, 0.25f, 0.0f, 1.5f, 0.1f),
-        RotatingSphere(GREEN, 0.5f, 1.0f, 1.5f, 0.1f),
-        RotatingSphere(BLUE, 0.75f, 2.0f, 1.5f, 0.1f)
-    };
-
-    DisableCursor();                    // Limit cursor to relative movement inside the window
+    std::vector<Point> points;
+    float speed = 0.1, angle=0;
+    int ticks_after_last_point, camera_mode=CAMERA_THIRD_PERSON;
 
     SetTargetFPS(60);                   // Set our example to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -52,24 +58,60 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())        // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        UpdateCamera(&camera, CAMERA_THIRD_PERSON);
-
+        // Shot new point if enough time spent
+        if (++ticks_after_last_point > 10) {
+            ticks_after_last_point = 0;
+            angle = (float) GetRandomValue(-30, 30) / 100;
+            Vector3 speed3 = {0.0, speed * cosf(angle), speed * sinf(angle)};
+            Vector3 boost3 = mul(speed3, 0.1);
+            points.push_back(Point(WHITE, 0.15f, {0, -5, 0}, speed3, boost3));
+        }
+        
+        // Change camera projection on left mouse click
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            if (camera.projection == CAMERA_PERSPECTIVE) {
+                camera.projection = CAMERA_ORTHOGRAPHIC;
+                camera.fovy = 25.0f;
+            }
+            else {
+                camera.projection = CAMERA_PERSPECTIVE;
+                camera.fovy = 50.0f;
+            }
+        }
+        
+        // Change camera mode on right mouse click
+        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+            if (camera_mode == CAMERA_THIRD_PERSON)
+                camera_mode = CAMERA_ORBITAL;
+            else
+                camera_mode = CAMERA_THIRD_PERSON;
+        }
+        UpdateCamera(&camera, camera_mode);
+        
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
-
+            ClearBackground(BLACK);
             BeginMode3D(camera);
-            
-            for (auto &sphere: spheres) {
-                sphere.move();
-                DrawSphere(sphere.position, sphere.radius, sphere.color);
-            }
 
-            DrawGrid(10, 1.0f);
+                // Draw points and delete them if they're out of cube
+                for (int i = 0; i < points.size(); i++) {
+                    points[i].move();
+                    if (fmax(fmax(fabs(points[i].position.x), fabs(points[i].position.y)), fabs(points[i].position.z)) >= 5)
+                        points.erase(std::next(points.begin(), i--));
+                    else
+                        DrawSphere(points[i].position, points[i].radius, points[i].color);
+                }
+                
+                // Fraw cube
+                DrawCubeWires((Vector3){0.0f, 0.0f, 0.0f}, 10.0f, 10.0f, 10.0f, WHITE);
+
+                // Draw axes and shot vector
+                DrawLine3D({0, -5, 0}, {5, -5, 0}, RED);
+                DrawLine3D({0, -5, 0}, {0, 0, 0}, GREEN);
+                DrawLine3D({0, -5, 0}, {0, -5, 5}, BLUE);
+                DrawLine3D({0, -5, 0}, {0, -5 + 5 * cosf(angle), 5 * sinf(angle)}, YELLOW);
 
             EndMode3D();
 
